aa7212ab1a1711a56f247875a05842c038bc7c5a
shubham singh rajput <rajputshubham7496@gmail.com>
2023-11-26 21:41:30 +0530

    cors added

--- src/main/java/com/example/Login/APIResponse1/LoginApplication.java
+++ src/main/java/com/example/Login/APIResponse1/LoginApplication.java
@@ -1,8 +1,5 @@
 package com.example.Login.APIResponse1;
 
-
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/example/Login/APIResponse1/LoginController.java
+++ src/main/java/com/example/Login/APIResponse1/LoginController.java
@@ -3,6 +3,7 @@ package com.example.Login.APIResponse1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.example.Login.Dto.LoginRequestDTO;
 import com.example.Login.Dto.SignUpRequestDTO;
 
 @Controller
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
 	
 	    @Autowired
